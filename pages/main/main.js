c915e4f5ce7a72cc46bbbe71d5b0220027d3ec16
zccguagua <532652152@qq.com>
2019-11-05 14:22:10 +0800

    小程序
    微信小程序

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,131 +1,153 @@
 // pages/main/main.js
+var weatherData = require('./data.js'); //引用外部的js文件
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    background: ['../image/mainpage/bg_2.jpg', '../image/mainpage/bg_3.jpg', '../image/mainpage/bg_1.jpg'],
+   /**
+    * 页面的初始数据
+    */
+   data: {
+      background: ['../image/mainpage/bg_2.jpg', '../image/mainpage/bg_3.jpg', '../image/mainpage/bg_1.jpg'],
 
-     /** 此处为日历自定义配置字段*/
-    calendarConfig: {
-      multi: true, /**  是否开启多选,*/
-      theme: 'default', /**  日历主题，目前共两款可选择，默认 default 及 elegant，自定义主题在 theme 文件夹扩展*/
-      showLunar: true, /**  是否显示农历，此配置会导致 setTodoLabels 中 showLabelAlways 配置失效*/
-      inverse: true, /**  单选模式下是否支持取消选中,*/
-      highlightToday: true, /**  是否高亮显示当天，区别于选中样式（初始化时当天高亮并不代表已选中当天）*/
-      takeoverTap: true, /**  是否完全接管日期点击事件（日期不会选中），配合 onTapDay() 使用*/
-      disablePastDay: true, /**  是否禁选当天之前的日期*/
-      firstDayOfWeek: 'Sun', /**  每周第一天为周一还是周日，默认按周日开始*/
-      showHandlerOnWeekMode: true /**  周视图模式是否显示日历头部操作栏，hideHeadOnWeekMode 优先级高于此配置*/
-    }
-  },
+      /** 此处为日历自定义配置字段*/
+      //  calendarConfig: {
+      //    multi: true, /**  是否开启多选,*/
+      //    theme: 'default', /**  日历主题，目前共两款可选择，默认 default 及 elegant，自定义主题在 theme 文件夹扩展*/
+      //    showLunar: true, /**  是否显示农历，此配置会导致 setTodoLabels 中 showLabelAlways 配置失效*/
+      //    inverse: true, /**  单选模式下是否支持取消选中,*/
+      //    highlightToday: true, /**  是否高亮显示当天，区别于选中样式（初始化时当天高亮并不代表已选中当天）*/
+      //    takeoverTap: true, /**  是否完全接管日期点击事件（日期不会选中），配合 onTapDay() 使用*/
+      //    disablePastDay: true, /**  是否禁选当天之前的日期*/
+      //    firstDayOfWeek: 'Sun', /**  每周第一天为周一还是周日，默认按周日开始*/
+      //    showHandlerOnWeekMode: true /**  周视图模式是否显示日历头部操作栏，hideHeadOnWeekMode 优先级高于此配置*/
+      //  }
+      // city: weatherData.weather_list.city,
+      // update_time: weatherData.weather_list.update_time,
+      // weather_list: weatherData.weather_list.data,
+      city: "",
+      update_time: "",
+      weather_list: "",
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
+   },
 
-  },
+   /**
+    * 生命周期函数--监听页面加载
+    */
+   onLoad: function(options) {
+      var that=this;
+      wx.request({
+         url: 'https://www.tianqiapi.com/api/?version=v1&appsecret=5lwqMTmq&appid=81398518',
+         header: {
+            'content-type': 'application/json'
+         },
+         success(res) {
+            console.log(res.data);
+            that.setData({
+               "city": res.data.city,
+               "update_time": res.data.update_time,
+               "weather_list": res.data.data,
+            })
+         }
+      })
+   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
+   /**
+    * 生命周期函数--监听页面初次渲染完成
+    */
+   onReady: function() {
 
-  },
+   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
+   /**
+    * 生命周期函数--监听页面显示
+    */
+   onShow: function() {
 
-  },
+   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
+   /**
+    * 生命周期函数--监听页面隐藏
+    */
+   onHide: function() {
 
-  },
+   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
+   /**
+    * 生命周期函数--监听页面卸载
+    */
+   onUnload: function() {
 
-  },
+   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
+   /**
+    * 页面相关事件处理函数--监听用户下拉动作
+    */
+   onPullDownRefresh: function() {
 
-  },
+   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
+   /**
+    * 页面上拉触底事件的处理函数
+    */
+   onReachBottom: function() {
 
-  },
+   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
+   /**
+    * 用户点击右上角分享
+    */
+   onShareAppMessage: function() {
 
-  },
+   },
 
-  /**
-   * 选择日期后执行的事件
-   * currentSelect 当前点击的日期
-   * allSelectedDays 选择的所有日期（当mulit为true时，allSelectedDays有值）
-   */
-  afterTapDay(e) {
-    console.log('afterTapDay', e.detail); // => { currentSelect: {}, allSelectedDays: [] }
-  },
-  /**
-   * 当日历滑动时触发(适用于周/月视图)
-   * 可在滑动时按需在该方法内获取当前日历的一些数据
-   */
-  onSwipe(e) {
-    console.log('onSwipe', e.detail);
-    const dates = this.calendar.getCalendarDates();
-  },
-  /**
-   * 当改变月份时触发
-   * => current 当前年月 / next 切换后的年月
-   */
-  whenChangeMonth(e) {
-    console.log('whenChangeMonth', e.detail);
-    // => { current: { month: 3, ... }, next: { month: 4, ... }}
-  },
-  /**
-   * 周视图下当改变周时触发
-   * => current 当前周信息 / next 切换后周信息
-   */
-  whenChangeWeek(e) {
-    console.log('whenChangeWeek', e.detail);
-    // {
-    //    current: { currentYM: {year: 2019, month: 1 }, dates: [{}] },
-    //    next: { currentYM: {year: 2019, month: 1}, dates: [{}] },
-    //    directionType: 'next_week'
-    // }
-  },
-  /**
-   * 日期点击事件（此事件会完全接管点击事件），需自定义配置 takeoverTap 值为真才能生效
-   * currentSelect 当前点击的日期
-   */
-  onTapDay(e) {
-    console.log('onTapDay', e.detail); // => { year: 2019, month: 12, day: 3, ...}
-  },
-  /**
-   * 日历初次渲染完成后触发事件，如设置事件标记
-   */
-  afterCalendarRender(e) {
-    console.log('afterCalendarRender', e);
-  }
+   /**
+    * 选择日期后执行的事件
+    * currentSelect 当前点击的日期
+    * allSelectedDays 选择的所有日期（当mulit为true时，allSelectedDays有值）
+    */
+   afterTapDay(e) {
+      console.log('afterTapDay', e.detail); // => { currentSelect: {}, allSelectedDays: [] }
+   },
+   /**
+    * 当日历滑动时触发(适用于周/月视图)
+    * 可在滑动时按需在该方法内获取当前日历的一些数据
+    */
+   onSwipe(e) {
+      console.log('onSwipe', e.detail);
+      const dates = this.calendar.getCalendarDates();
+   },
+   /**
+    * 当改变月份时触发
+    * => current 当前年月 / next 切换后的年月
+    */
+   whenChangeMonth(e) {
+      console.log('whenChangeMonth', e.detail);
+      // => { current: { month: 3, ... }, next: { month: 4, ... }}
+   },
+   /**
+    * 周视图下当改变周时触发
+    * => current 当前周信息 / next 切换后周信息
+    */
+   whenChangeWeek(e) {
+      console.log('whenChangeWeek', e.detail);
+      // {
+      //    current: { currentYM: {year: 2019, month: 1 }, dates: [{}] },
+      //    next: { currentYM: {year: 2019, month: 1}, dates: [{}] },
+      //    directionType: 'next_week'
+      // }
+   },
+   /**
+    * 日期点击事件（此事件会完全接管点击事件），需自定义配置 takeoverTap 值为真才能生效
+    * currentSelect 当前点击的日期
+    */
+   onTapDay(e) {
+      console.log('onTapDay', e.detail); // => { year: 2019, month: 12, day: 3, ...}
+   },
+   /**
+    * 日历初次渲染完成后触发事件，如设置事件标记
+    */
+   afterCalendarRender(e) {
+      console.log('afterCalendarRender', e);
+   }
 
 
 })
--- pages/map/map.js
+++ pages/map/map.js
@@ -3,210 +3,292 @@ var _animation;
 var _animationIndex = 1;
 var app = getApp();
 var param = {
-  data: {
-    animation: '',
-    latitude: "",
-    longitude: "",
-    scale: 14,
-    circles: [],
-    markers: [{
+   data: {
+      animation: '',
       latitude: "",
       longitude: "",
-    }],
-    Positions: [{
-      id: 1001,
-      latitude: 31.9464,
-      longitude: 117.3966,
-      name: '安中医',
-      infomation: ""
-    }, {
-      id: 1002,
-      latitude: 30.122,
-      longitude: 118.175,
-      name: '黄山',
-      infomation: ""
-    }, {
-      id: 1003,
-      latitude: 39.915035,
-      longitude: 116.403936,
-      name: '天安门',
-      infomation: ""
-    }, {
-      id: 1004,
-      latitude: 29.9782,
-      longitude: 86.9222,
-      name: '珠穆朗玛峰',
-      infomation: ""
-    }, {
-      id: 1005,
-      latitude: 29.653,
-      longitude: 91.118,
-      name: '布达拉宫',
-      infomation: ""
-    }, {
-      id: 1006,
-      latitude: 35.358,
-      longitude: 138.731,
-      name: '富士山',
-      infomation: ""
-    }, {
-      id: 1007,
-      latitude: 38.891,
-      longitude: -77.019,
-      name: '美国白宫',
-      infomation: ""
-    }, {
-      id: 1008,
-      latitude: 48.8587,
-      longitude: 2.2934,
-      name: '埃菲尔铁塔',
-      infomation: ""
-    }, {
-      id: 1009,
-      latitude: 0,
-      longitude: 0,
-      name: '北极',
-      infomation: ""
-    }]
-  },
+      scale: 14,
+      circles: [],
+      markers: [{
+         latitude: "",
+         longitude: "",
+      }],
+      Positions: [{
+            id: 1001,
+            latitude: 31.9464,
+            longitude: 117.3966,
+            name: '安中医',
+            infomation: ""
+         }, {
+            id: 1002,
+            latitude: 30.122,
+            longitude: 118.175,
+            name: '黄山',
+            infomation: ""
+         },
+         //   {
+         //    id: 1003,
+         //    latitude: 39.915035,
+         //    longitude: 116.403936,
+         //    name: '天安门',
+         //    infomation: ""
+         //  },
+         {
+            id: 1101,
+            latitude: 36.584302,
+            longitude: 100.502975,
+            name: '青海湖',
+            infomation: ""
+         },
+         {
+            id: 1102,
+            latitude: 29.574322,
+            longitude: 103.356274,
+            name: '峨眉山',
+            infomation: ""
+         },
+         {
+            id: 1103,
+            latitude: 48.512898,
+            longitude: 87.136529,
+            name: '喀纳斯',
+            infomation: ""
+         },
+         {
+            id: 1104,
+            latitude: 25.153166,
+            longitude: 110.438262,
+            name: '漓江',
+            infomation: ""
+         },
+         {
+            id: 1104,
+            latitude: 27.140138,
+            longitude: 100.210479,
+            name: '玉龙雪山',
+            infomation: ""
+         },
+         {
+            id: 1004,
+            latitude: 29.9782,
+            longitude: 86.9222,
+            name: '珠穆朗玛峰',
+            infomation: ""
+         }, {
+            id: 1005,
+            latitude: 29.653,
+            longitude: 91.118,
+            name: '布达拉宫',
+            infomation: ""
+         }, {
+            id: 1006,
+            latitude: 35.358,
+            longitude: 138.731,
+            name: '富士山',
+            infomation: ""
+         },
+         //   {
+         //    id: 1007,
+         //    latitude: 38.891,
+         //    longitude: -77.019,
+         //    name: '美国白宫',
+         //    infomation: ""
+         //  }, 
+         //  {
+         //    id: 1008,
+         //    latitude: 48.8587,
+         //    longitude: 2.2934,
+         //    name: '埃菲尔铁塔',
+         //    infomation: ""
+         //  }, 
+         //  {
+         //    id: 1009,
+         //    latitude: 0,
+         //    longitude: 0,
+         //    name: '北极',
+         //    infomation: ""
+         //  }
+      ]
+   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-    var that = this;
-    wx.getLocation({
-      type: 'wgs84',
-      success: function(res) {
-        var latitude = res.latitude
-        var longitude = res.longitude
-        var speed = res.speed
-        var accuracy = res.accuracy
-        console.log("纬度：" + latitude + ",经度:" + longitude);
-        that.setData({
-          longitude: res.longitude,
-          latitude: res.latitude
-        })
-        that.setData({
-          'markers[0].longitude': res.longitude,
-          'markers[0].latitude': res.latitude
-        })
-        that.setData({
-          circles: [{
-            latitude: res.latitude,
-            longitude: res.longitude,
-            color: '#7cb5eccc',
-            fillColor: '#7cb5ec88',
-            radius: 400,
-            strokeWidth: 0.2
-          }]
-        })
+   /**
+    * 生命周期函数--监听页面加载
+    */
+   onLoad: function(options) {
+      var that = this;
+
+      wx.getSetting({
+         success: (res) => {
+            console.log(res);
+            console.log(res.authSetting['scope.userLocation']);
+            if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {
+               //非初始化进入该页面,且未授权         
+               wx.showModal({
+                  title: '是否授权当前位置',
+                  content: '需要获取您的地理位置，请确认授权，否则无法获取地址',
+                  success: function(res) {
+                     if (res.cancel) {
+                        console.info("1授权失败返回数据");
+                        // refused();
+                     } else if (res.confirm) {
+                        wx.openSetting({
+                           success: function(data) {
+                              console.log(data);
+                              if (data.authSetting["scope.userLocation"] == true) {
+                                 wx.showToast({
+                                    title: '授权成功',
+                                    icon: 'success',
+                                    duration: 2000
+                                 })
+                              } else {
+                                 wx.showToast({
+                                    title: '授权失败',
+                                    icon: 'none',
+                                    duration: 2000
+                                 });
+                              }
+                           }
+                        })
+                     }
+                  }
+               })
+            } else if (res.authSetting['scope.userLocation'] == true) {
+               console.log("权限成功");
+               this.calculateDistance();
+            } else if (res.authSetting['scope.userLocation'] == undefined) {
+               //初始化进入          
+               console.log("权限成功");
+               this.calculateDistance();
+            }
+         }
+      })
 
 
-        var datas = that.data.Positions;
-        console.log(datas.length);
-        for (var i = 0; i < datas.length; i++) {
-          var po = datas[i];
-          var tt = that.getDistance(latitude, longitude, po.latitude, po.longitude, po.name);
-          // console.log(tt);
-          po["infomation"] = tt;
-        }
-        that.setData({
-          Positions: datas
-        });
 
-        // console.log(datas);
 
+
+
+   },
+
+   /**
+    * 生命周期函数--监听页面初次渲染完成
+    */
+   onReady: function() {
+
+   },
+
+
+   /**
+    * 生命周期函数--监听页面卸载
+    */
+   onUnload: function() {
+
+   },
+   getDistance: function(lat1, lng1, lat2, lng2, position) {
+
+      lat1 = lat1 || 0;
+
+      lng1 = lng1 || 0;
+
+      lat2 = lat2 || 0;
+
+      lng2 = lng2 || 0;
+
+      var rad1 = lat1 * Math.PI / 180.0;
+
+      var rad2 = lat2 * Math.PI / 180.0;
+
+      var a = rad1 - rad2;
+
+      var b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
+
+      var r = 6378137;
+
+      var ss = (r * 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(rad1) * Math.cos(rad2) * Math.pow(Math.sin(b / 2), 2)))).toFixed(0);
+      if ((ss / 1000) < 0.5) {
+         console.log(position + "就在您的附近");
+         return position + "就在您的附近";
+      } else {
+         console.log("您距离" + position + ":" + (ss / 1000) + "公里");
+         return "您距离" + position + ":" + (ss / 1000) + "公里";
       }
-    }, true, 4000)
+   },
+   Refresh: function() {
+      _animation = wx.createAnimation({
+         duration: 200,
+         timingFunction: 'linear', // "linear","ease","ease-in","ease-in-out","ease-out","step-start","step-end"
+         delay: 0,
+         transformOrigin: '50% 50% 0'
+      });
+      _animation.rotate(360 * _animationIndex).step();
+      _animationIndex++;
+      this.setData({
+         animation: _animation.export()
+      });
+      this.onLoad();
+   },
+   Magnify: function() {
+      var th_scale = this.data.scale;
+      if (th_scale <= 18) {
+         th_scale++;
+      }
+      this.setData({
+         scale: th_scale
+      });
+   },
+   Reduce: function() {
+      var th_scale = this.data.scale;
+      if (th_scale >= 5) {
+         th_scale--;
+      }
+      this.setData({
+         scale: th_scale
+      });
+   },
+   calculateDistance: function() {
+      var that = this;
+      wx.getLocation({
+         type: 'wgs84',
+         success: function(res) {
+            var latitude = res.latitude
+            var longitude = res.longitude
+            var speed = res.speed
+            var accuracy = res.accuracy
+            console.log("纬度：" + latitude + ",经度:" + longitude);
+            that.setData({
+               longitude: res.longitude,
+               latitude: res.latitude
+            });
+            that.setData({
+               'markers[0].longitude': res.longitude,
+               'markers[0].latitude': res.latitude
+            });
+            that.setData({
+               circles: [{
+                  latitude: res.latitude,
+                  longitude: res.longitude,
+                  color: '#7cb5eccc',
+                  fillColor: '#7cb5ec88',
+                  radius: 400,
+                  strokeWidth: 0.2
+               }]
+            });
+            var datas = that.data.Positions;
+            console.log(datas.length);
+            for (var i = 0; i < datas.length; i++) {
+               var po = datas[i];
+               var tt = that.getDistance(latitude, longitude, po.latitude, po.longitude, po.name);
+               // console.log(tt);
+               po["infomation"] = tt;
+            }
+            that.setData({
+               Positions: datas
+            });
 
+            // console.log(datas);
 
-
-
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-  getDistance: function(lat1, lng1, lat2, lng2, position) {
-
-    lat1 = lat1 || 0;
-
-    lng1 = lng1 || 0;
-
-    lat2 = lat2 || 0;
-
-    lng2 = lng2 || 0;
-
-    var rad1 = lat1 * Math.PI / 180.0;
-
-    var rad2 = lat2 * Math.PI / 180.0;
-
-    var a = rad1 - rad2;
-
-    var b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
-
-    var r = 6378137;
-
-    var ss = (r * 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(rad1) * Math.cos(rad2) * Math.pow(Math.sin(b / 2), 2)))).toFixed(0);
-    if ((ss / 1000) < 0.5) {
-      console.log(position + "就在您的附近");
-
-
-      return position + "就在您的附近";
-
-    } else {
-      console.log("您距离" + position + ":" + (ss / 1000) + "公里");
-      return "您距离" + position + ":" + (ss / 1000) + "公里";
-
-    }
-
-  },
-  Refresh: function() {
-    _animation = wx.createAnimation({
-      duration: 200,
-      timingFunction: 'linear', // "linear","ease","ease-in","ease-in-out","ease-out","step-start","step-end"
-      delay: 0,
-      transformOrigin: '50% 50% 0'
-    });
-    _animation.rotate(360 * _animationIndex).step();
-    _animationIndex++;
-    this.setData({
-      animation: _animation.export()
-    });
-    this.onLoad();
-  },
-  Magnify: function() {
-    var th_scale = this.data.scale;
-    if (th_scale <= 18) {
-      th_scale++;
-    }
-    this.setData({
-      scale: th_scale
-    });
-  },
-  Reduce: function() {
-    var th_scale = this.data.scale;
-    if (th_scale >= 5) {
-      th_scale--;
-    }
-    this.setData({
-      scale: th_scale
-    });
-  }
-
-
+         }
+      }, true, 4000);
+   }
 };
 Page(param);
